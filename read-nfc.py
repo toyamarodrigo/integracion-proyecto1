6563bec533c6fb5af69c874e47f01b76057912cf
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-09-19 06:08:24 -0300

    Espanol a Ingles

--- read-nfc.py
+++ read-nfc.py
@@ -6,7 +6,7 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-		print("Colocar Tarjeta a leer")
+		print("Place your tag near the reader")
 		id,text = reader.read()
 		print(id)
 		print(text)
